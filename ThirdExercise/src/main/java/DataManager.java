cb3997b5611e7a7d0d294d859d012b95a4bb7cc1
Evelin Szabados <szabadosevelin7@gmail.com>
2020-08-06 19:54:21 +0200

    Implement getSeriesAverageDurationPerType query

--- ThirdExercise/src/main/java/DataManager.java
+++ ThirdExercise/src/main/java/DataManager.java
@@ -31,12 +31,31 @@ public class DataManager {
         return busiestMachines.get(3);
     }
 
-    public String getBusiestDay() {
-        return "Busiest day is: ";
+    public String getBusiestDay() throws SQLException {
+
+        String query = "SELECT DATE_FORMAT(study_datetime,'%Y-%m-%d') as day," +
+                "COUNT(*) as count FROM study ORDER BY count DESC LIMIT 1;";
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        rs.first();
+        return rs.getDate("day").toString();
     }
 
-    public String getAverageSeriesDurationPerSeriesType() {
-        return "Average series duration:";
+    public String getAverageSeriesDurationPerSeriesType() throws SQLException {
+
+        String query = "SELECT series_type, " +
+                "TRUNCATE(AVG(series_duration),2) AS average " +
+                "FROM serie " +
+                "GROUP BY series_type;";
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(query);
+        StringBuilder result = new StringBuilder("Average series duration: \n");
+
+        while (rs.next()) {
+            result.append(rs.getString("series_type"))
+                    .append(": ").append(rs.getDouble("average")).append("\n");
+        }
+        return result.toString();
     }
 
     public String getDataTypeWithHighestDiagnostic() {
